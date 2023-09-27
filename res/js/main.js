629a377ff189117d828217f6197581d7d41deada
honzafuka <jan.fuka@student.spsmb.cz>
2023-09-27 10:32:43 +0200

    added upgrade button

--- res/js/main.js
+++ res/js/main.js
@@ -1,6 +1,6 @@
 //console.log; - vypisuje do konzole
 console.log("ahoj světe");
-
+/*
 let myNumber = 16;
 console.log(myNumber);
 myNumber = 4;
@@ -10,12 +10,13 @@ const PI = 3.14;
 console.log(PI);
 PI = 4;
 console.log(PI)
-
+*/
 
 const dog = document.getElementById("dog");
 const counter = document.getElementById("counter");
-let numberOfCookies
-const upgrade = document.getElementById("upgrade")
+let numberOfCookies = 0;
+const upgrade = document.getElementById("upgrade");
+let clickIncrease = 1;
 //.onclick - na kliknuti
 // () => arrow funkce
 // {} - scope
@@ -24,9 +25,21 @@ dog.onclick = () => {
     //numberOfCookies = numberOfCookies + 1;
     //numberOfCookies += 1;     
     //++ pricte jednicku - inkrement
-    numberOfCookies++;
+    numberOfCookies += clickIncrease;
     console.log(numberOfCookies);
-    counter.innerHTML = "Number of robux: " + numberOfCookies;
+    counter.innerHTML =  numberOfCookies;
 
 }
+
+upgrade.onclick = () => {
+    if (numberOfCookies >= 50) {
+        //odectu cenu upgradu
+        numberOfCookies = numberOfCookies - 50;
+        //numberOFCookies = numberOfCookies - 50
+        counter.innerHTML =  numberOfCookies;
+        //zvednu pocet na kliknuti
+        clickIncrease++;
+    }
+}
+
 
